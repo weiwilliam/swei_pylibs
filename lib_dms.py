616bc410c5b0dbcdd5982cfe4227546dded53c3a
weiwilliam <swei@albany.edu>
2022-12-18 07:17:45 +0800

    expand the dict for vertical layers

--- lib_dms.py
+++ lib_dms.py
@@ -7,10 +7,31 @@ def read_dms(dmskey):
     return tmp
 
 def find_dms_longname(nametag):
-    longname_dict = {
+    longname_sfc_dict = {
             'S00300':'Net radiation flux at surface (W/m^2)',
             'S00310':'Net shortwave (solar) flux at the surface (W/m^2)',
             'S00320':'Net longwave (infrared) flux at the surface (W/m^2)',
             }
-    longname = longname_dict[nametag]
+    longname_layer_dict = {
+            '550':'Cloud liquid water mixing ratio(kg/kg)',
+            '551':'Cloud water mixing ratio(kg/kg)',
+            '552':'Cloud ice mixing ratio(kg/kg)',
+            '553':'Rain mixing ratio(kg/kg)',
+            '554':'Snow mixing ratio(kg/kg)',
+            '555':'Graupel mixing ratio(kg/kg)',
+            '556':'Water vapor mixing ratio(kg/kg)',
+            }
+    if 'S00' == nametag[:3]:
+        longname = longname_sfc_dict[nametag]
+    else:
+        vartag = nametag[3:]
+        if 'M' == nametag[0]:
+            modellv = nametag[1:3] 
+            longname = longname_layer_dict[vartag] + ' at level '+modellv
+        else:
+            plv = nametag[:3]
+            if 'H' == plv[0]:
+                plv = '1000'
+            longname = longname_layer_dict[vartag] + ' at '+ plv + ' hPa'
+    
     return longname
