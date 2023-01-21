46bdb8fab13dc4a26cd5595ef4de10a6fcbceed6
weiwilliam <swei@albany.edu>
2023-01-22 07:45:58 +0800

    add S00100 to dmslib

--- lib_dms.py
+++ lib_dms.py
@@ -7,31 +7,33 @@ def read_dms(dmskey):
     return tmp
 
 def find_dms_longname(nametag):
-    longname_sfc_dict = {
+    longname_singlelyr_dict = {
+            'S00100':'Surface temperature (K)',
             'S00300':'Net radiation flux at surface (W/m^2)',
             'S00310':'Net shortwave (solar) flux at the surface (W/m^2)',
             'S00320':'Net longwave (infrared) flux at the surface (W/m^2)',
+            'X00770':'Total cloudiness (0.-1.0)',
             }
-    longname_layer_dict = {
-            '550':'Cloud liquid water mixing ratio(kg/kg)',
-            '551':'Cloud water mixing ratio(kg/kg)',
-            '552':'Cloud ice mixing ratio(kg/kg)',
-            '553':'Rain mixing ratio(kg/kg)',
-            '554':'Snow mixing ratio(kg/kg)',
-            '555':'Graupel mixing ratio(kg/kg)',
-            '556':'Water vapor mixing ratio(kg/kg)',
+    longname_vertlyr_dict = {
+            '550':'Cloud liquid water mixing ratio (kg/kg)',
+            '551':'Cloud water mixing ratio (kg/kg)',
+            '552':'Cloud ice mixing ratio (kg/kg)',
+            '553':'Rain mixing ratio (kg/kg)',
+            '554':'Snow mixing ratio (kg/kg)',
+            '555':'Graupel mixing ratio (kg/kg)',
+            '556':'Water vapor mixing ratio (kg/kg)',
             }
-    if 'S00' == nametag[:3]:
-        longname = longname_sfc_dict[nametag]
+    if 'S00' == nametag[:3] or 'X00' == nametag[:3]:
+        longname = longname_singlelyr_dict[nametag]
     else:
         vartag = nametag[3:]
         if 'M' == nametag[0]:
             modellv = nametag[1:3] 
-            longname = longname_layer_dict[vartag] + ' at level '+modellv
+            longname = longname_vertlyr_dict[vartag] + ' at level '+modellv
         else:
             plv = nametag[:3]
             if 'H' == plv[0]:
                 plv = '1000'
-            longname = longname_layer_dict[vartag] + ' at '+ plv + ' hPa'
+            longname = longname_vertlyr_dict[vartag] + ' at '+ plv + ' hPa'
     
     return longname
